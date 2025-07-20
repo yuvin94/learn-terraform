7e7a80cd59dd5cbb45678356cbbb563f73e811f6
Yuvin Perera <yuvinperera@MacBookPro.modem>
2025-07-20 23:06:27 +1000

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -3,13 +3,13 @@
 
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  /*cloud {
+    organization = "Yuvin-test"
 
     workspaces {
       name = "learn-terraform"
     }
-  }
+  }*/
 
   required_providers {
     aws = {
